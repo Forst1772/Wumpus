7e0c757731d765446bc45f23c176efd50c7a7517
Forst1772 <jlqc1772@gmail.com>
2018-05-17 18:26:59 -0500

    Inclusión de abismos con viento

--- Wumpus.cpp
+++ Wumpus.cpp
@@ -4,36 +4,31 @@
 using namespace std;
 int **mapa = NULL;
 /* logica del mapa
-	terreno = 0
-	
-	Wumpus = 5
-	olor wumpus = -4
-	
-	abismo = 3
-	viento = -2
+terreno = 0
 
-	viento con olor = -6
-	olor con abismo = -1
+Wumpus = 5
+olor wumpus = -4
 
-	jugador = 1
+abismo = 3
+viento = -2
+
+viento con olor = -6
+olor con abismo = -1
+
+jugador = 1
 */
-
 void efecto(int x1, int y1, int tipo){
-	mapa[x1][y1] = mapa[x1][y1] - tipo;
+	if(mapa[x1][y1] == -2){
+		if (tipo == -4)
+			mapa[x1][y1] = mapa[x1][y1] - tipo;
+	} else if(mapa[x1][y1] == -4){
+		if(tipo == -2)
+			mapa[x1][y1] = mapa[x1][y1] - tipo;
+	} else if (mapa[x1][y1] != 3){
+		mapa[x1][y1] = mapa[x1][y1] - tipo;
+	}
 }
-
-void wumpus(int dimension){
-	int caracter = 4; // olor
-	int x1, y1;
-	do{
-		x1 = rand()%dimension;
-		y1 = rand()%dimension;
-	}while(mapa[x1][y1] == 3);
-	
-	cout<< "(" << x1+1 << "," << y1+1 << ")\n";
-	
-	mapa[x1][y1] = 5;
-	
+void olorViento(int x1, int y1, int caracter, int dimension){
 	if(x1 != 0)
 		efecto(x1-1,y1,caracter);
 	if(y1 != 0)
@@ -62,11 +57,53 @@ void wumpus(int dimension){
 			efecto(x1+1,y1+1,caracter);
 	}
 }
+void abismos(int cantidad){
+	int dimension = cantidad;
+	int a = dimension - ((double) (dimension*30)/100);
+	cantidad = cantidad / 2;
+	
+	while(cantidad != 0){
+		
+		int n = 2 + rand()%a;
+		
+		int x = rand()%dimension;
+		int y = rand()%dimension;
+		
+		mapa[x][y] = 3;
+		olorViento(x,y,2,dimension);
+		if (x != (dimension-1) && x !=0 && y != (dimension-1) && y !=0){
+			while(n != 0){
+				int x1 = (rand()%4) - 2 ;
+				int y1 = (rand()%4) - 2;
+				
+				if(mapa[abs(x+x1)][abs(y+y1)] != 3){
+					mapa[abs(x+x1)][abs(y+y1)] = 3;
+					olorViento(abs(x+x1),abs(y+y1),2,dimension);
+				}
+				n--;
+			}
+		}		
+		cantidad--;
+	}
+}
+
+void wumpus(int dimension){
+	int x1, y1;
+	do{
+		x1 = rand()%dimension;
+		y1 = rand()%dimension;
+	}while(mapa[x1][y1] == 3);
+	
+	cout<< "(" << x1+1 << "," << y1+1 << ")\n";
+	
+	mapa[x1][y1] = 5;
+	olorViento(x1,y1,4,dimension);
+}
 
 int main(int argc, char *argv[]) {
 	srand(time(NULL));
-	int n;
-	cin >> n;
+	int n=10;
+	//cin >> n;
 	
 	if (n >= 10 && n <= 20){
 		mapa = new int*[n];
@@ -81,10 +118,10 @@ int main(int argc, char *argv[]) {
 			}
 		}
 		
+		abismos(n);
 		
-		//LLenar abismos
 		
-		wumpus(n);
+		//wumpus(n);
 		
 		//poner personaje
 		
